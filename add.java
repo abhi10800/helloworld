a4ecbd08a42606e2ea1a75506c6ec3e6d7c710cc
abhi10800 <102384158+abhi10800@users.noreply.github.com>
2022-07-08 17:51:30 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class add
+{
+public static void main(String args[]) 
+{
+    Scanner obj=new Scanner(System.in);
+    int a,b,c;
+    System.out.println("enter a number");
+    a=obj.nextInt();
+    System.out.println("enter an another number");
+    b=obj.nextInt();
+    c=a+b;
+    System.out.printIn("sum"+c);
+}
+
+}
+
+
